69978d7287c47b97579cfa4befbb7c02680a3166
Nicole Colabella <ncolabella@fubo.tv>
2022-04-13 15:59:16 -0400

    add style-dictionary flow

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Wed, 13 Apr 2022 19:49:40 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
